02ab95acedb188cc269fd681283cc3a5f74e0188
Hans Foster <fosterhans@gmail.com>
2019-10-30 04:43:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,6 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |seasons, season_values|
     puts "#{seasons.capitalize}:"
     season_values.each do |holidays, holiday_values|
+      holidays_array = []
       holidays_array = holidays.split("_")
       holidays_array.each do |words|
         words.capitalize
